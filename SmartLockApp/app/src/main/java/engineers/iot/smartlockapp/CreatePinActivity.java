a393b6b7871f0ca5f3d0a331e201fc81f96c8c9f
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-03-09 14:37:05 +0200

    initialized UI elements in a CreatePinActivity java class

--- SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/CreatePinActivity.java
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/CreatePinActivity.java
@@ -1,6 +1,9 @@
 package engineers.iot.smartlockapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +11,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class CreatePinActivity extends AppCompatActivity {
 
+    private EditText username, pinCode, confirmCode;
+    private Button create;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_pin);
 
+        initViews();
+
+        handleActions();
+
+    }
+
+    private void initViews() {
+        username = findViewById(R.id.username);
+        pinCode = findViewById(R.id.code);
+        confirmCode = findViewById(R.id.confirmCode);
+        create = findViewById(R.id.create);
+    }
+
+    private void handleActions() {
+     create.setOnClickListener(e -> {
+         Toast.makeText(this, "Pressed create button", Toast.LENGTH_SHORT).show();
+     });
     }
 }
