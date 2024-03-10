5c1dff6ed09f76d856e04f9f61c8674580419b54
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-03-10 18:43:27 +0200

    created an authentication activity and it's java class

---
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/AuthenticationActivity.java
@@ -0,0 +1,17 @@
+package engineers.iot.smartlockapp;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+    }
+}
