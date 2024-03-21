2e415e60266ce7645ef04cdd48323bb597cd3f0c
BSC-COM-NE-16-19 <95578397+BSC-COM-NE-16-19@users.noreply.github.com>
2024-03-21 19:29:49 +0200

    History activity Layouts

---
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/HistoryActivity.java
@@ -0,0 +1,19 @@
+package engineers.iot.smartlockapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+    }
+}
