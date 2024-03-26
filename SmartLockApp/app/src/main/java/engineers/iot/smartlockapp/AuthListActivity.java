763c75bce8241f642c9a4594d3ede9af09948782
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-03-26 10:56:18 +0200

    created variable for adapter, recycle view and array list

--- SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/AuthListActivity.java
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/AuthListActivity.java
@@ -1,18 +1,30 @@
 package engineers.iot.smartlockapp;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import engineers.iot.smartlockapp.Adapter.Adapter;
+import engineers.iot.smartlockapp.Model.User;
+
 
 public class AuthListActivity extends AppCompatActivity {
+
+    private ImageView back;
+    private RecyclerView recyclerView;
+    private Adapter recycleViewAdapter;
+    private ArrayList<User> userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_list);
     }
+
+
 }
