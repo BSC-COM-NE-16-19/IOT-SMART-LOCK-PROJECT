f16354f7b70ebe706676bfbe726a0c1af44e2470
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-03-26 11:27:54 +0200

    added events handing function

--- SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/AuthListActivity.java
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/AuthListActivity.java
@@ -5,6 +5,7 @@ import android.widget.ImageView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -24,7 +25,33 @@ public class AuthListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_list);
+
+        initViews();
+
+        handEvents();
     }
 
+    private void initViews() {
+        back = findViewById(R.id.back);
+        recyclerView = findViewById(R.id.recycleView);
+        userList = new ArrayList<>();
+
+        getUserData();
+
+        recycleViewAdapter = new Adapter(getApplicationContext(),userList);
+        recyclerView.setAdapter(recycleViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false));
+    }
+
+    private void getUserData() {
+        userList.add(new User("Godbless"," ", 49869));
+        userList.add(new User("Blessing"," ", 59598));
+        userList.add(new User("Moddy"," ", 48585));
+        userList.add(new User("Dr. Kim"," ", 57876));
+    }
+
+    private  void handEvents(){
+        back.setOnClickListener(e-> onBackPressed());
+    }
 
 }
