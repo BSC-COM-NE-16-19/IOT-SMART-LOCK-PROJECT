54d3420d853ec2c1b9c1feba3d12d2dd7f592a87
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-03-26 10:38:02 +0200

    created an adapter class for a recycle view

---
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/Adapter/Adapter.java
@@ -0,0 +1,58 @@
+package engineers.iot.smartlockapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import engineers.iot.smartlockapp.Model.User;
+import engineers.iot.smartlockapp.R;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    private final Context context;
+    private final ArrayList<User> userList;
+    public Adapter(Context context,ArrayList<User> userList){
+        this.context = context;
+        this.userList = userList;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.auth_list_items,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView name, id;
+        private final RelativeLayout layout;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.name);
+            id = itemView.findViewById(R.id.id);
+            layout = itemView.findViewById(R.id.layout);
+
+
+        }
+    }
+}
