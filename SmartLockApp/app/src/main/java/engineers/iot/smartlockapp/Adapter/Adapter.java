0047c195b14fabb159321ca25f55f8220ac513a7
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-03-28 16:37:23 +0200

    initiated and created the dialog box object

--- SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/Adapter/Adapter.java
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/Adapter/Adapter.java
@@ -1,23 +1,28 @@
 package engineers.iot.smartlockapp.Adapter;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+
+
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import engineers.iot.smartlockapp.Model.User;
 import engineers.iot.smartlockapp.R;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     private final Context context;
+    private Dialog dialog;
     private final ArrayList<User> userList;
     public Adapter(Context context,ArrayList<User> userList){
         this.context = context;
@@ -32,13 +37,22 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         return new ViewHolder(view);
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
         holder.name.setText(userList.get(position).getFirstName());
         holder.id.setText(String.valueOf(userList.get(position).getId()));
 
-        holder.layout.setOnClickListener(e-> Toast.makeText(context, "Pressed user: " + userList.get(position).getFirstName(), Toast.LENGTH_SHORT).show());
+        holder.layout.setOnClickListener(e-> {
+            dialog = new Dialog(context);
+            dialog.setContentView(R.layout.update_user_dialog_box);
+            Objects.requireNonNull(dialog.getWindow()).setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setBackgroundDrawable(context.getDrawable(R.drawable.white_card_view_style));
+            dialog.show();
+
+
+        });
 
     }
 
