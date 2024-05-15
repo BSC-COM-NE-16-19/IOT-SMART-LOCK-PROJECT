6842c374a94e2c0e95dc36ee63eafa4bc9f654a0
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-05-16 00:17:21 +0200

    created radio button variable and handled their events

--- SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/StatesActivity.java
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/StatesActivity.java
@@ -5,10 +5,17 @@ import android.widget.ImageView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatRadioButton;
+
+import engineers.iot.smartlockapp.Database.ConnectDB;
 
 public class StatesActivity extends AppCompatActivity {
 
     private ImageView back;
+
+    private ConnectDB database;
+
+    private AppCompatRadioButton auth, enroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +27,26 @@ public class StatesActivity extends AppCompatActivity {
     }
 
     private void initViews(){
+
         back = findViewById(R.id.back);
+        database = new ConnectDB("STATUS");
+        auth = findViewById(R.id.authentication);
+        enroll = findViewById(R.id.enrollment);
     }
 
     private void handleActions() {
+        auth.setOnClickListener(e-> {
+               if(auth.isChecked()) {
+                   database.getDatabaseReference().child("sensorStatus").setValue("Authentication");
+               }
+                });
+
+        enroll.setOnClickListener(e-> {
+            if(enroll.isChecked()) {
+                database.getDatabaseReference().child("sensorStatus").setValue("Enrollment");
+            }
+        });
+
         back.setOnClickListener(e-> onBackPressed());
     }
 
