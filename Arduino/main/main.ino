85a33476d4194eaddae5aaa84a0430e1e17de146
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-05-25 10:17:44 +0200

    imported libraries

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,9 +1,16 @@
+#include <WiFiManager.h>
+#include <FingerPrint.h> 
+#include <WiFi.h>
+#include <String.h>
 
+#include <Firebase_ESP_Client.h>
+#include <addons/TokenHelper.h>
+#include <addons/RTDBHelper.h>
 
 void setup() {
 
 }
 
 void loop() {
-  
+
 }
