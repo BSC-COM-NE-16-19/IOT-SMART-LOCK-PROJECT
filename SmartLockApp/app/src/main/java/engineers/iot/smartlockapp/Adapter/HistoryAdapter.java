e146a4ecae0b60037fe1e3774d2959d268e36d64
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-05-30 22:03:02 +0200

    implemented the history adapter class

--- SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/Adapter/HistoryAdapter.java
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/Adapter/HistoryAdapter.java
@@ -1,4 +1,59 @@
 package engineers.iot.smartlockapp.Adapter;
 
-public class HistoryAdapter {
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+import engineers.iot.smartlockapp.Model.History;
+import engineers.iot.smartlockapp.R;
+
+public class HistoryAdapter extends FirebaseRecyclerAdapter<History, HistoryAdapter.ViewHolder> {
+
+    public HistoryAdapter(@NonNull FirebaseRecyclerOptions<History> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ViewHolder viewHolder, int i, @NonNull History history) {
+
+        if(history.getStatus().equals("Accessed")) {
+            viewHolder.status.setTextColor(Color.GREEN);
+        } else if(history.getStatus().equals("Denied")) {
+            viewHolder.status.setTextColor(Color.RED);
+        }
+
+        viewHolder.name.setText(history.getName());
+
+        viewHolder.time.setText(history.getTime());
+
+        viewHolder.status.setText(history.getStatus());
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_layout,parent,false);
+        return new ViewHolder(view);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView name, time, status;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.name);
+            time = itemView.findViewById(R.id.time);
+            status = itemView.findViewById(R.id.status);
+        }
+    }
 }
