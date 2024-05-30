a3be7d3c117b9dc80eb446512e22abf2b59ec467
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-05-30 22:45:02 +0200

    created an variables and initialized them

--- SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/HistoryActivity.java
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/HistoryActivity.java
@@ -4,10 +4,20 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+import engineers.iot.smartlockapp.Adapter.HistoryAdapter;
+import engineers.iot.smartlockapp.Database.ConnectDB;
+import engineers.iot.smartlockapp.Model.History;
 
 public class HistoryActivity extends AppCompatActivity {
 
     private ImageView back;
+
+    private HistoryAdapter historyAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +30,37 @@ public class HistoryActivity extends AppCompatActivity {
     }
 
     private void initViews(){
+
         back = findViewById(R.id.back);
+        RecyclerView historyRecycleView = findViewById(R.id.historyRecycleView);
+
+        ConnectDB database = new ConnectDB("USER");
+
+        historyRecycleView.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false));
+
+        FirebaseRecyclerOptions<History> options =
+                new FirebaseRecyclerOptions.Builder<History>()
+                        .setQuery(database.getDatabaseReference(), History.class)
+                        .build();
+
+
+        historyAdapter = new HistoryAdapter(options);
+        historyRecycleView.setAdapter(historyAdapter);
     }
 
     private void handleActions() {
         back.setOnClickListener(e-> onBackPressed());
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        historyAdapter.stopListening();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        historyAdapter.startListening();
+    }
 }
