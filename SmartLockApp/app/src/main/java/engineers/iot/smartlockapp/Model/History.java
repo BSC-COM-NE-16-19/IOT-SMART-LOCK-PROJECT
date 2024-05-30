136ad9f3b8c991664966a0f3cdb19c59c951307e
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-05-30 20:48:06 +0200

    implemented the history model class

--- SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/Model/History.java
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/Model/History.java
@@ -2,5 +2,35 @@ package engineers.iot.smartlockapp.Model;
 
 public class History {
 
+    private String name, time, status;
 
+    public History(String name, String time, String status) {
+        this.name = name;
+        this.time = time;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
