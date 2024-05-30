6720954f98c48e591feb078e3887db045a841e0a
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-05-30 23:34:46 +0200

    created an activity for already have a code

---
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/AlreadyHaveCode.java
@@ -0,0 +1,14 @@
+package engineers.iot.smartlockapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlreadyHaveCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
