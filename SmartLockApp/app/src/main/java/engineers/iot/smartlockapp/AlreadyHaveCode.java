e78c23b1c2d3cb6ac2b44405915268a70370c7cc
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-05-31 16:16:58 +0200

    created functions for initiating variables and handling actions and implemented the functions

--- SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/AlreadyHaveCode.java
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/AlreadyHaveCode.java
@@ -1,14 +1,35 @@
 package engineers.iot.smartlockapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AlreadyHaveCode extends AppCompatActivity {
 
+    private Button continues;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_already_have_code);
 
+        initViews();
+
+        handleActions();
+
+    }
+
+    private void initViews() {
+        continues = findViewById(R.id.continues);
+    }
+
+    private void handleActions() {
+        continues.setOnClickListener(e-> {
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
