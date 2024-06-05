bebbeca9b00caa2dea836b5b7f961801a3c39450
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-06-06 00:07:07 +0200

    added a event listener for added home owner to firebase

--- SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/CreatePinActivity.java
+++ SmartLockApp/app/src/main/java/engineers/iot/smartlockapp/CreatePinActivity.java
@@ -8,12 +8,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import engineers.iot.smartlockapp.Database.ConnectDB;
+import engineers.iot.smartlockapp.Model.HomeOwner;
 
 
 public class CreatePinActivity extends AppCompatActivity {
 
     private EditText username, pinCode, confirmCode;
+
+    private ConnectDB db;
 
     private TextView already;
     private Button create;
@@ -39,9 +48,29 @@ public class CreatePinActivity extends AppCompatActivity {
 
     private void handleActions() {
      create.setOnClickListener(e -> {
-         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-         startActivity(intent);
-         finish();
+
+         String name = username.getText().toString();
+         String code = pinCode.getText().toString();
+         String confPin = confirmCode.getText().toString();
+
+         if(name.isEmpty() || code.isEmpty() || confPin.isEmpty()) {
+             Toast.makeText(this, "Please fill all details!!", Toast.LENGTH_SHORT).show();
+         } else if(!code.equals(confPin)) {
+             Toast.makeText(this, "The code do not match!!", Toast.LENGTH_SHORT).show();
+         } else {
+             HomeOwner owner = new HomeOwner(name,code, " ");
+             db = new ConnectDB("HOMEOWNER");
+             db.getDatabaseReference().child(name.toLowerCase()).setValue(owner).addOnCompleteListener(new OnCompleteListener<Void>() {
+                 @Override
+                 public void onComplete(@NonNull Task<Void> task) {
+                     Toast.makeText(CreatePinActivity.this, "Successfully created a code", Toast.LENGTH_SHORT).show();
+                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                     startActivity(intent);
+                     finish();
+                 }
+             });
+         }
+
      });
 
      already.setOnClickListener(e-> {
