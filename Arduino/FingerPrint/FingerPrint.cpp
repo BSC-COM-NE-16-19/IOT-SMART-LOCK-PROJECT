a0cf70be699cb0a92738614d8b682fb25d3eccb1
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-06-12 18:06:48 +0200

    implemented display fingerprint properties method

--- Arduino/FingerPrint/FingerPrint.cpp
+++ Arduino/FingerPrint/FingerPrint.cpp
@@ -349,6 +349,35 @@ void FingerPrint::checkFingerPrintSensor() {
 
 }
 
+void FingerPrint::checkFingerPrintSensor() {
+  Serial.begin(9600);
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  Serial.println("\n\nAdafruit finger detect test");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  delay(5);
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1) { delay(1); }
+  }
+
+}
+
+void FingerPrint::displayFingerPrintProperties() {
+  Serial.println(F("Reading sensor parameters"));
+  finger.getParameters();
+  Serial.print(F("Status: 0x")); Serial.println(finger.status_reg, HEX);
+  Serial.print(F("Sys ID: 0x")); Serial.println(finger.system_id, HEX);
+  Serial.print(F("Capacity: ")); Serial.println(finger.capacity);
+  Serial.print(F("Security level: ")); Serial.println(finger.security_level);
+  Serial.print(F("Device address: ")); Serial.println(finger.device_addr, HEX);
+  Serial.print(F("Packet len: ")); Serial.println(finger.packet_len);
+  Serial.print(F("Baud rate: ")); Serial.println(finger.baud_rate);
+}
 
 void FingerPrint::setEnrollment(bool enrollment) {
   _enrollment = enrollment;
