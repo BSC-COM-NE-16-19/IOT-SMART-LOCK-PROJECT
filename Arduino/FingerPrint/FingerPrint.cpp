3c9dceeace6277c5e1e23b0280079880ad214d4b
bsc-com-ne-02-19 <bsc-com-ne-02-19@unima.ac.mw>
2024-06-12 17:49:18 +0200

    created a fingerprint cpp file for implement the fingerprint hearder file

---
+++ Arduino/FingerPrint/FingerPrint.cpp
@@ -0,0 +1,15 @@
+#include "FingerPrint.h"
+
+#define MODEM_RX 16
+#define MODEM_TX 17
+#define mySerial Serial2
+#define DELAY 2000
+#define pin 2
+#define buzzlePin 23
+
+int attempts = 0;
+int buzzleDelay = 5000;
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+LCD_I2C lcd = LCD_I2C(0x27, 16, 2);
