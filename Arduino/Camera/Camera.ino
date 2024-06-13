5390a8ba7e1a7ffa2cfed902e15a871dc6e73e87
Modester620 <bsc-com-ne-01-18@unima.ac.mw>
2024-06-13 19:50:46 +0200

    Update Camera.ino
    library addition

--- Arduino/Camera/Camera.ino
+++ Arduino/Camera/Camera.ino
@@ -1,7 +1,8 @@
-/* this code is for the camera module
-   for capturing images and for live streaming
-   on the mobile application
-   */#include <Arduino.h>
+//code to send a picture of the intruder to the owner of the building after 3 wrong attempts of unlocking the door
+//through telegram
+
+//including necessary libraries
+#include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include "soc/soc.h"
@@ -10,3 +11,7 @@
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 #include <String.h>
+
+#include <Firebase_ESP_Client.h>
+#include <addons/TokenHelper.h>
+#include <addons/RTDBHelper.h>
