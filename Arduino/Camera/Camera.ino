58711b27393fdcc2d367328ecbaa5e2deb229542
Modester620 <bsc-com-ne-01-18@unima.ac.mw>
2024-06-13 19:24:36 +0200

    Update Camera.ino
    Camera sensor libraries

--- Arduino/Camera/Camera.ino
+++ Arduino/Camera/Camera.ino
@@ -1,4 +1,12 @@
 /* this code is for the camera module
    for capturing images and for live streaming
    on the mobile application
-   */
+   */#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_camera.h"
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+#include <String.h>
