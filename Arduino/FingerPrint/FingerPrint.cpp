9868136993c34afd8440ad6289534812dab81ae5
BSC-COM-NE-16-19 <95578397+BSC-COM-NE-16-19@users.noreply.github.com>
2024-06-14 17:00:58 +0200

    Reading  fingerprint sensor parameters

--- Arduino/FingerPrint/FingerPrint.cpp
+++ Arduino/FingerPrint/FingerPrint.cpp
@@ -405,7 +405,15 @@ void FingerPrint::checkFingerPrintSensor() {
 
 }
 void FingerPrint::displayFingerPrintProperties() {
-
+Serial.println(F("Reading sensor parameters"));
+  finger.getParameters();
+  Serial.print(F("Status: 0x")); Serial.println(finger.status_reg, HEX);
+  Serial.print(F("Sys ID: 0x")); Serial.println(finger.system_id, HEX);
+  Serial.print(F("Capacity: ")); Serial.println(finger.capacity);
+  Serial.print(F("Security level: ")); Serial.println(finger.security_level);
+  Serial.print(F("Device address: ")); Serial.println(finger.device_addr, HEX);
+  Serial.print(F("Packet len: ")); Serial.println(finger.packet_len);
+  Serial.print(F("Baud rate: ")); Serial.println(finger.baud_rate);
 }
 
 // Enroll
