f66dccc1705ca74754bcc59b42d6f2f90600611d
Bsc-com-ne-17-19 <98205318+Bsc-com-ne-17-19@users.noreply.github.com>
2024-06-14 03:24:16 +0300

    Sms alert

--- Arduino/FingerPrint/FingerPrint.cpp
+++ Arduino/FingerPrint/FingerPrint.cpp
@@ -467,6 +467,9 @@ uint8_t FingerPrint::getFingerprintID() {
 
     attempts++;
     if(attempts == 3) {
+      // Configure the A9G module for SMS
+      sendCommand("AT+CMGF=1", 3000); // Set SMS to text mode
+      sendSMS("+265990409624", "\"Alert: UNAUTHORISED INDIVIDUAL IS ATTEMPTETING TO ACCESS YOUR LOCK. PLEASE CHECK YOUR TELEGRAM FOR A PICTURE.\""); // Sending SMS to the specified number
       digitalWrite(buzzlePin,HIGH);
       delay(buzzleDelay);
       digitalWrite(buzzlePin,LOW);
